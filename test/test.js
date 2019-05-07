604c41578e3c366d7b662e0a4f4de2845ac01618
Florent <florent@sqooba.io>
2019-05-07 15:22:25 +0200

    Fixed test

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ var normalizeNewline = require('normalize-newline');
 var read             = require('read-file-relative').readSync;
 var createReport     = require('./utils/create-report');
 
-it('Should produce report with colors', function () {
+it('Should produce a basic report', function () {
     var report   = createReport(true);
-    var expected = read('./data/report-with-colors.xml');
+    var expected = read('./data/simple-case.xml');
 
     report   = normalizeNewline(report).trim();
     expected = normalizeNewline(expected).trim();
@@ -13,12 +13,3 @@ it('Should produce report with colors', function () {
     assert.strictEqual(report, expected);
 });
 
-it('Should produce report without colors', function () {
-    var report   = createReport(false);
-    var expected = read('./data/report-without-colors.xml');
-
-    report   = normalizeNewline(report).trim();
-    expected = normalizeNewline(expected).trim();
-
-    assert.strictEqual(report, expected);
-});
